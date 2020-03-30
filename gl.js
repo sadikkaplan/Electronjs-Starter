a9bbee83b372f598a8e64c82d64d23e6b45846c5
Nurbaki Vural <nurbakivural@gmail.com>
2020-03-30 14:04:32 +0300

    fix

--- gl.js
+++ gl.js
@@ -1,9 +1,9 @@
-const THREE = require("three");
 
 //Burdaki renderer WebGL klasına ait
-var scene = new Scene();
-var camera = new PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1, 1000);
-var renderer = new WebGLRenderer();
+//Namespace THREE önemliymiş
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
 
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize(window.innerWidth, window.innerHeight);
